de6f0e8e209160ce723bda0c06d27035da57ea4b
dolsotbap <leejeongro1992@gmail.com>
2021-06-19 21:21:27 +0900

    feat: loader(css,style,file,url)

--- practice/src/app.js
+++ practice/src/app.js
@@ -1,4 +1,13 @@
-import * as math from "./math(module)"; //방법1
-// import {sum} from './math.js';//방법2
+// import * as math from "./math(module)"; //방법1
+// // import {sum} from './math.js';//방법2
 
-console.log(math.sum(1, 2));
+// console.log(math.sum(1, 2));
+
+import "./app.css"; //방법1
+import nyancat from "./nyancat.jpeg"; //방법2
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `
+    <img src='${nyancat}' />
+  `;
+});
--- practice/webpack.config.js
+++ practice/webpack.config.js
@@ -13,8 +13,17 @@ module.exports = {
     rules: [
       {
         //로더는
-        test: /\.js$/, //.js 확장자를 가진 파일마다(이 패턴에 걸리는 애들마다)
-        use: [path.resolve("./my-webpack-loader.js")], //이 파일을 실행함
+        test: /\.css$/, //.js 확장자를 가진 파일마다(이 패턴에 걸리는 애들마다)
+        use: ["style-loader", "css-loader"], //이 파일을 실행함 //여러개인경우 배열의 뒤에서부터 순서대로 실행
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/", //앞에 붙일 경로
+          name: "[name].[ext]?[hash]", //이름.확장자명 + 쿼리(hash)
+          limit: 20000, //20kb //2kb미만의 파일은 base64 문자열로 변환하여 main.js로 들어가고, 그 이상은 file-loader를 통해 dist에 복사한다
+        },
       },
     ],
   },
